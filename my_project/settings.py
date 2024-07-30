f9e6316eaa167542e806d996414ee7eb2c29a9c4
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-07-30 10:50:07 +0000

    reset initial setup

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jh!vv#q^c9hs&jkl42tomr-#c0$j*=o7c!&xdj(456wfjv7fc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-juanchitopi-amigoblogfi-e12nif7seaj.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    #own application
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
