ca844b1b3b2a7d8d1e3629cedf6aff4f5f82e284
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-08-01 10:49:33 +0000

    test users app views

--- my_project/urls.py
+++ my_project/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
 
     # user authentications
     path("accounts/", include("allauth.urls")),
-    # path('register/', user_views.register, name="register"),
-    # path('profile/', user_views.profile, name="profile"),
+    path('register/', user_views.register, name="register"),
+    path('profile/', user_views.profile, name="profile"),
     # path('profile/profile_update/', user_views.profile_update, name="profile-update"),
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout")
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Successfully Created for {username} Login In Now!!!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
