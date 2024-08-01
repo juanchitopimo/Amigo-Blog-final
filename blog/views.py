dc37a6c9bce7e1bb2cf42e7e1a512634bd381009
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-08-01 09:02:17 +0000

    define and fix  parameters in views and models

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -23,3 +23,20 @@ class PostListView(LoginRequiredMixin, ListView):
     ordering = ["-date_posted"]
 
 
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # def test_func(self):
+    #     post = self.get_object()
+    #     if self.request.user == post.author:
+    #         return True
+    #     return False
