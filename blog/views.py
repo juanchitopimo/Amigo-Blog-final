2271d3fc7b4629a1a76bf8a446a1c1d8d8ef6eb2
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-08-01 12:55:52 +0000

    add delete class to blog/views and cretae further post_confirm_delete html template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -49,6 +49,16 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
