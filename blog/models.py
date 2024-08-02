60a237aa730ac729b9502d59d88c2b27478d3ed3
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-08-02 13:04:38 +0000

    add media media root to setting to create media directory

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,27 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog-detail', kwargs={'pk': self.pk})
+
+#         ---------
+# from django.db import models
+# from django.contrib.auth.models import User
+# from django.utils import timezone
+# from django.urls import reverse
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     date_posted = models.DateTimeField(default=timezone.now)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     excerpt = models.TextField(blank=True, null=True)
+
+#     def __str__(self):
+#         return self.title
+
+#     def get_absolute_url(self):
+#         return reverse('blog-detail', kwargs={'pk': self.pk})
+
+#     def save(self, *args, **kwargs):
+#         if not self.excerpt:
+#             self.excerpt = self.content[:100]  # First 100 characters of content
+#         super().save(*args, **kwargs)
