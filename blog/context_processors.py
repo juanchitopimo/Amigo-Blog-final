cdeb55d0cb81d70a1ac7f15615fec8f6c73ae02e
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-08-12 21:06:57 +0000

    update sidebar of Most commented post, with context_processor feature to automaticly get the latest most commeted post

---
+++ blog/context_processors.py
@@ -0,0 +1,7 @@
+# blog/context_processors.py
+from .models import Post
+from django.db.models import Count
+
+def most_commented_posts(request):
+    most_commented = Post.objects.annotate(comment_count=Count('comments')).order_by('-comment_count')[:5]
+    return {'most_commented_posts': most_commented}
