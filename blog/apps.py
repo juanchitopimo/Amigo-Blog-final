91c541f669cc34911148b47230919d0af5687044
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-08-13 20:15:21 +0000

    include confidential info in gitignore file and update CSS style for titles

--- blog/apps.py
+++ blog/apps.py
@@ -1,11 +1,3 @@
-# from django.apps import AppConfig
-
-# class BlogConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'blog'
-
-#     def ready(self):
-#         import blog.signals  # Import the signals to connect them
 from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
