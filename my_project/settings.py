790b1560ccbc9e24f5b3dc697e9591e6cdafd588
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-09-27 11:35:59 +0000

    update hero image and debbugerd

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-juanchitopi-amigoblogfi-e12nif7seaj.ws.codeinstitute-ide.net', '.herokuapp.com']
+    '8000-juanchitopi-amigoblogfi-k70hus11fu2.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
